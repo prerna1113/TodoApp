b5f1253598ed8e75e55e274510d5262a2de8f6e7
Pre-11 <prernakumari7427@gmail.com>
2023-08-20 23:18:05 +0530

    state added

--- todo-app/src/Component/Todo.jsx
+++ todo-app/src/Component/Todo.jsx
@@ -1,21 +1,57 @@
 
 import { Box, Heading, Input } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './CSS/Todo.module.css'
 
 function Todo() {
+const [state,setState] = useState('')
+console.log(state);
     return (
         <Box className={styles.box}>
             <Box className={styles.input}>
 
-                <Heading as='h2' size='xl'>
+                <Box 
+                className={styles.container}
+                >
+                <Heading 
+                className={styles.heading}
+
+                as='h2' size='lg'>
                    TODO APP
                 </Heading>
 
+                <p
+                className={styles.subHeading}
+                // as= 'h9' size = 'xs'
+                >
+                    by Prerna Dubey
+
+                </p>
+                
+
+                </Box>
+
+               
+                <Box>
                 <Input
-                    className={styles.inputBox}
-                    type="text"
-                    placeholder="Write Something" />
+                    style={{
+                        backgroundColor: "white",
+                        borderRadius:"14px",
+                        height:"55px",
+                        fontSize:"28px",
+                        marginTop:"15px"
+                    }}
+                   
+                   
+                   placeholder="write something"
+                    onChange={(event)=>{setState({
+                        ...state,
+                        name:event.target.value
+                    })}}
+                    />
+                </Box>
+
+               
 
 
 
