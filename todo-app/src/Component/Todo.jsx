3fd9a1cc75112058dab82a1c246fadbbbdc7a90a
Pre-11 <prernakumari7427@gmail.com>
2023-08-20 22:41:20 +0530

    header added

--- todo-app/src/Component/Todo.jsx
+++ todo-app/src/Component/Todo.jsx
@@ -1,33 +1,32 @@
 
-import { Box, Input } from '@chakra-ui/react'
+import { Box, Heading, Input } from '@chakra-ui/react'
 import React from 'react'
 import styles from './CSS/Todo.module.css'
 
 function Todo() {
-  return (
-   <Box  className={styles.box}>
+    return (
+        <Box className={styles.box}>
+            <Box className={styles.input}>
+
+                <Heading as='h2' size='xl'>
+                   TODO APP
+                </Heading>
+
+                <Input
+                    className={styles.inputBox}
+                    type="text"
+                    placeholder="Write Something" />
 
 
-    <Box className={styles.input}>
-        <Input
-        style={{
-            height:"55px",
-            borderRadius:"14px",
-            backgroundColor:"white",
-            fontSize:"28px"
-        }}
-        type="text"
-        placeholder="Write Something"
-         />
-        
 
-    </Box>
-   
-   </Box>
-       
 
-    
-  )
+            </Box>
+
+        </Box>
+
+
+
+    )
 }
 
 export default Todo
