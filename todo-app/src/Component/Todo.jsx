a384dfc07584535ddbb172f7839719d42d7056e9
Pre-11 <prernakumari7427@gmail.com>
2023-08-20 22:25:39 +0530

    searchBar completed

--- todo-app/src/Component/Todo.jsx
+++ todo-app/src/Component/Todo.jsx
@@ -1,12 +1,32 @@
 
+import { Box, Input } from '@chakra-ui/react'
 import React from 'react'
+import styles from './CSS/Todo.module.css'
 
 function Todo() {
   return (
-    <div>
-        Hello Todo
+   <Box  className={styles.box}>
 
-    </div>
+
+    <Box className={styles.input}>
+        <Input
+        style={{
+            height:"55px",
+            borderRadius:"14px",
+            backgroundColor:"white",
+            fontSize:"28px"
+        }}
+        type="text"
+        placeholder="Write Something"
+         />
+        
+
+    </Box>
+   
+   </Box>
+       
+
+    
   )
 }
 
